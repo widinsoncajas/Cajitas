37eea05ba62c97a67637540ffb1fbb10c74a46b9
widinsoncajas <adriancajas2002@gmail.com>
2025-04-30 00:31:05 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -36,8 +36,7 @@ app.get('GAMA_FAMILIAR/GAMA_FAMI.php', (req, res) => {
   });
 });
 
-// Configuración para servir archivos estáticos (imágenes, CSS, JS)
-app.use('/GAMA_FAMILIAR', express.static(path.join(__dirname, 'GAMA_FAMILIAR')));
+
 
 // Puerto donde el servidor escuchará
 const port = process.env.PORT || 3000;
